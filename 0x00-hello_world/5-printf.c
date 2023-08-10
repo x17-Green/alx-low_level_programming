8b3420fe5eabf304271f40dc29b390205c6d3cfa
x17-green <x17.ebi@icloud.com>
2023-08-10 19:57:41 +0100

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
