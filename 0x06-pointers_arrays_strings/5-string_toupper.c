23e56657c27e234712d03f3907d544939aff8ef3
x17-green <x17.ebi@icloud.com>
2023-08-27 14:20:19 +0100

    Function to change text cases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - function to change text character cases
+ * @c: string containing characters
+ * Return: ch
+ */
+char *string_toupper(char *c)
+{
+	int num;
+
+	for (num = 0; c[num] != '\0'; num++)
+	{
+		if (c[num] > 98 && c[num] < 123)
+		{
+			c[num] -= 32;
+		}
+	}
+	return (c);
+}
