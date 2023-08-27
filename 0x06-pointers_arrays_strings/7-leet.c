18770f10c0d7a0f3eb6b8abf112a45b0a93aa649
x17-green <x17.ebi@icloud.com>
2023-08-27 14:53:06 +0100

    Function to replace character encoded into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * leet - function to encode a character to 1337
+ * @r: a string to replace characters
+ * Return: encoded `r` string
+ */
+char *leet(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		while (s[n] == 'a' || s[n] == 'A')
+		{
+			s[n] = '4';
+		}
+		while (s[n] == 'e' || s[n] == 'E')
+		{
+			s[n] = '3';
+		}
+		while (s[n] == 'o' || s[n] == 'O')
+		{
+			s[n] = '0';
+		}
+		while (s[n] == 't' || s[n] == 'T')
+		{
+			s[n] = '7';
+		}
+		while (s[n] == 'l' || s[n] == 'L')
+		{
+			s[n] = '1';
+		}
+	}
+	return (s);
+}
