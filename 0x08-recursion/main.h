449e6ff47ba5c2681b9da4df45a1b191347d3fd0
x17-green <x17.ebi@icloud.com>
2023-09-26 04:38:23 +0100

    Function that compares two strings to be considered identical

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,3 +12,5 @@ int hai(int n, int d);
 int is_palindrome(char *s);
 int get_length(char *s);
 int my_pal(char *s, int l);
+int wildcmp(char *s1, char *s2);
+int findsrc(char *s, char c, int i, int p);
