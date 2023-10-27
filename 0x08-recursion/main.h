fd1da6e98fcefb1c8c12f751c72e7d4b47a88f43
x17-green <x17.ebi@icloud.com>
2023-10-27 04:32:46 +0100

    Updated header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,7 +11,8 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 int is_prime_number(int n);
 int check_prime(int n, int i);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
 int is_palindrome(char *s);
-int check_palindrome(char *s);
 
 #endif
