91a01dea640a56f7732929cb2abaf0a1e7abc393
x17-green <x17.ebi@icloud.com>
2023-10-28 15:25:30 +0100

    Program that prints its name followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include <stdio.o>
+#include <stdio.h>
 /**
  * main - Function to relay program name
  * @argc: Argument count
@@ -8,6 +8,22 @@
  */
 int main(int argc, char *argv[])
 {
+	int num;
+
 	printf("%s\n", argv[0]);
+
+	if (argc > 1)
+	{
+		for (num = 1; num < argc; num++)
+		{
+			printf("%s[%d] = %s\n", argv[0], num, argv[num]);
+		}
+	}
+	/**
+	 * else
+	 * {
+	 * printf("No other arguments left.\n");
+	 * }
+	 */
 	return (0);
 }
