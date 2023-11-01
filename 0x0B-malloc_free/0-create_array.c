3624013d4e52a7bc69324b57d4fd291a33f7ef6e
x17-green <x17.ebi@icloud.com>
2023-11-01 04:57:48 +0100

    Function that creates an array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,50 +10,23 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *arr;
+	unsigned int i = 0;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *arr = (char *) malloc(size * sizeof(char));
+	arr = (char *) malloc(size * sizeof(char));
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-
-	unsigned int i;
-
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
 	return (arr);
-}
-/**
- * main - Recursive function
- *
- * Return: 1 if pointer to array or NULL, otherwise 0
- */
-int main(void)
-{
-	char c = 'a';
-	unsigned int size = 10;
-	char *arr = create_array(size, c);
-
-	if (arr == NULL)
-	{
-		printf("Memory allocation failed\n");
-		return (1);
-	}
-
-	unsigned int i;
-
-	for (i = 0; i < size; i++)
-	{
-		printf("%c ", arr[i]);
-	}
-
-	free(arr);
-	return (0);
 }
