ba53d1aeaa8e08dcbc73f30bce052b7db22fbed6
x17-green <x17.ebi@icloud.com>
2023-11-02 05:33:02 +0100

    A program that returns 2-dimensional array of int

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * alloc_grid - Function returns 2 dimensional array
+ * of integers
+ * @width: Width of the arrays
+ * @height: Height of the arrays
+ *
+ * Return: NULL on failure, or if width and height is <= 0
+ * otherwise, return a pointer to 2-demensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
