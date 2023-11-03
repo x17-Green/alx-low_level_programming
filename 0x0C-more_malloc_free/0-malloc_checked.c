2c9731d580f004a5a7b2da6ea953adef061079e7
x17-green <x17.ebi@icloud.com>
2023-11-03 04:32:04 +0100

    Function that allocates a memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * malloc_checked - Function allocates memory using malloc
+ * @b: Unsigned int variable to allocate
+ *
+ * Return: Memory location
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		perror("Failed to allocate memory");
+		exit(98);
+	}
+	return (mem);
+}
