41c58ed5c5edea971e5526c2a85ceab738d8dad7
x17-green <x17.ebi@icloud.com>
2023-11-10 05:25:56 +0100

    A dog is the only thing

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ *
+ *
+ */
+int main(void)
+{
+	struct dog *d;
+
+	d = &my_dog;
+
+	return (0);
+}
