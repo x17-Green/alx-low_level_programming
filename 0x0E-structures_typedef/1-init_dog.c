ae3bc446b15fc1480fbeed6cc40d7acfcef7fc40
x17-green <x17.ebi@icloud.com>
2023-11-10 06:21:46 +0100

    Function that initialize struct data variables

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,25 +1,15 @@
 #include "dog.h"
-#include <stdio.h>
 /**
+ * init_dog - Function for dog structure
  *
- *
- *
+ * @d: Pointer to struct data
+ * @name: Char data struct type
+ * @age: Float struct data type
+ * @owner: Char struct data type
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-/**
- *
- *
- */
-int main(void)
-{
-	struct dog *d;
-
-	d = &my_dog;
-
-	return (0);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
