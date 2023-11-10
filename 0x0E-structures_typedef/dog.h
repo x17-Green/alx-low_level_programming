0e85cc4d1bead42031b3b2be0ce2691060b61bd8
x17-green <x17.ebi@icloud.com>
2023-11-10 04:58:39 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * struct dog - Dog structure
+ * @name: Name category
+ * @age: Age category
+ * @owner: Owners category
+ *
+ * Dog Structures: this is for a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
