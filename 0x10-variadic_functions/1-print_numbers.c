e1b45f9a0e87e0fbeb83db0bba250c46a75a07e8
x17-green <x17.ebi@icloud.com>
2023-11-18 18:12:18 +0100

    Function that prints number with a separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,13 +3,15 @@
  * print_numbers - prints numbers with a separator
  * @separator: the string to be printed between numbers
  * @n: the number of integers passed to the function
- * @ap: the variadic argument list
  *
  * Return: void
  */
-void print_numbers(const char *separator, const unsigned int n, va_list ap)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int i;
+
+	va_start(args, n);
 
 	if (n == 0)
 	{
@@ -19,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, va_list ap)
 
 	for (i = 0; i < n; i++)
 	{
-		int num = va_arg(ap, int);
+		int num = va_arg(args, int);
 
 		printf("%d", num);
 		if (separator != NULL && i < n - 1)
@@ -28,20 +30,6 @@ void print_numbers(const char *separator, const unsigned int n, va_list ap)
 		}
 	}
 	printf("\n");
-}
 
-/**
- * print_numbers - wrapper for print_numbers function
- * @separator: the string to be printed between numbers
- * @n: the number of integers passed to the function
- *
- * Return: void
- */
-void print_numbers(const char *separator, const unsigned int n, ...)
-{
-	va_list args;
-
-	va_start(args, n);
-	print_numbers(separator, n, args);
 	va_end(args);
 }
